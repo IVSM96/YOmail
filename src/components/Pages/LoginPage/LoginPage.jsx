f40cfff84c7168be2583876e444b577bf76e90fe
ivsm <ivsm.project@yandex.ru>
2022-09-26 18:03:09 +0300

    "доделана форма в Login"

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -2,22 +2,52 @@ import React from "react"
 import "./LoginPage.css"
 
 
-export const LoginPage = () => {
-    const currentUser = true
-    return(
-    
+export class LoginPage extends React.Component {
+    currentUser = true
+
+    constructor() {
+        super()
+        this.state = {
+            adress: '',
+            password: '',
+        }
+
+        this.handlerSubmit = this.handlerSubmit.bind(this)
+    }
+
+    handlerSubmit(event) {
+        const newUser = {
+            adress: this.state.adress,
+            password: this.state.password
+        }
+        //Здесь будет отправляться на бэк
+        console.log(newUser)
+        event.preventDefault()
+    }
+
+render() {
+    return(   
         <div className="wrapper_login">
-            {currentUser ? (
-            <form className="login">
+            {this.currentUser ? (
+            <form className="login" onSubmit={this.handlerSubmit}>
                 <div className="login_logo"><span>YO</span>mail</div>
-                <input type="text" placeholder="Введите адресс"></input>
-                <input placeholder="Введите пароль" type="password"></input>
+                <input
+                    type="text"
+                    placeholder="Введите адресс"
+                    value={this.state.adress}
+                    onChange={(event) => this.setState({adress: event.target.value})}>
+                </input>
+                <input 
+                    placeholder="Введите пароль" 
+                    type="password"
+                    value={this.state.password}
+                    onChange={(event) => this.setState({password: event.target.value})}></input>
                 <button className="login_button" type="submit">Войти</button>
             </form>
             ):(
             <form className="login">
                 <div><span>YO</span>mail</div>
-                <input disabled={currentUser} value={currentUser.adress} type="text"></input>
+                <input disabled={this.currentUser} value={this.currentUser.adress} type="text"></input>
                 <input placeholder="Введите пароль" type="password"></input>
                 <button type="submit">Зарегистрироваться</button>
             </form>   
@@ -25,4 +55,6 @@ export const LoginPage = () => {
            <img className="login_exit" src="./icons/close.svg" alt=""></img>
         </div>
     )
+}
+    
 }
